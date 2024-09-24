f01600617d7db5e616d69bdb3230b1e580bbf05f
Ryan Holanda <ryanholanda08@gmail.com>
2024-09-24 16:05:08 -0300

    chore: add function to initialize the package for testing porpouses

--- lib/web3kit.dart
+++ lib/web3kit.dart
@@ -1,5 +1,8 @@
 library web3kit;
 
+import "package:flutter/foundation.dart";
+import "package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart";
+import "package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart";
 import "package:web3kit/src/inject.dart";
 
 export "core/core.dart";
@@ -9,7 +12,15 @@ export "ui/ui.dart";
 class Web3Kit {
   Web3Kit();
 
-  /// Initializes the client to be able to use it.
+  /// Initializes the package to be able to use it.
   /// This function should be called only once
   static Future<void> initialize() async => await Inject.setup();
+
+  /// initializes the package for running tests
+  @visibleForTesting
+  static Future<void> initializeForTest() async {
+    SharedPreferencesAsyncPlatform.instance = InMemorySharedPreferencesAsync.empty();
+
+    await initialize();
+  }
 }
