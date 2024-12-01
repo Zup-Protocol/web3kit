018723ac22393b12d3641f2bad68fae003105349
Ryan Holanda <ryanholanda08@gmail.com>
2024-12-01 18:50:01 -0300

    fix: parse token amount from num with incorrect values

--- lib/core/extensions/num_extension.dart
+++ lib/core/extensions/num_extension.dart
@@ -1,4 +1,11 @@
+import "package:decimal/decimal.dart";
+
 extension NumExtension on num {
   /// Convert a token amount from its common decimals representation to its EVM representation (e.g 1 -> 1000000000000000000)
-  BigInt parseTokenAmount({required int decimals}) => BigInt.from(this) * BigInt.from(10).pow(decimals);
+  BigInt parseTokenAmount({required int decimals}) {
+    final base = Decimal.parse(toString());
+    final exponent = Decimal.fromInt(10).pow(decimals).toDecimal();
+
+    return (base * exponent).toBigInt();
+  }
 }
--- test/core/extensions/num_extension_test.dart
+++ test/core/extensions/num_extension_test.dart
@@ -5,4 +5,18 @@ void main() {
   test("`parseTokenAmount` should correctly convert a dart num to an EVM decimal-like", () {
     expect(9200.parseTokenAmount(decimals: 18), BigInt.from(9200e18));
   });
+
+  test("When passing double values to `parseTokenAmount`, it should correctly convert them to an EVM decimal-like", () {
+    expect(0.5.parseTokenAmount(decimals: 18), BigInt.from(0.5e18));
+  });
+
+  test(
+      "When passing increible huge values to `parseTokenAmount`, it should correctly convert them to an EVM decimal-like",
+      () {
+    const decimals = 18;
+    expect(
+      2e200.parseTokenAmount(decimals: decimals),
+      BigInt.from(2) * BigInt.from(10).pow(200 + decimals),
+    );
+  });
 }
