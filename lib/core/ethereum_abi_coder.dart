42dfe9f12673e21d2bc15623a6f58a1094969f2c
Ryan Holanda <ryanholanda08@gmail.com>
2025-06-11 18:07:24 +0200

    change static functions from abi coder to allow mocking

--- lib/core/ethereum_abi_coder.dart
+++ lib/core/ethereum_abi_coder.dart
@@ -4,19 +4,36 @@ import "package:web3kit/src/js/ethers/ethers_abi_coder.js.dart";
 import "package:web3kit/src/js/ethers/ethers_solidity_packed.js.dart";
 import "package:web3kit/src/mocks/package_mocks/js_interop_mock.dart" if (dart.library.html) "dart:js_interop";
 
-class EthereumEncoder {
-  static String encodePacked(List<String> types, List<dynamic> values) {
+/// Encoding and decoding functions for Ethereum ABI data
+/// Work for encoding and decoding any type of data supported by the Ethereum ABI,
+/// working the same as the Solidity compiler (abi.encode;abi.decode, etc.)
+class EthereumAbiCoder {
+  /// Encode a list of values to a packed ABI encoded string.
+  ///
+  /// Accepts a list of types and a list of values and returns a packed ABI encoded string.
+  /// Works the same as Solidity's `abi.encodePacked` function.
+  ///
+  /// What's packed encoding? Concatenate values in their minimal binary form without padding them to 32 bytes
+  ///
+  /// E.g encoding a uint8: 10 -> 0x0a
+  String encodePacked(List<String> types, List<dynamic> values) {
     return ethersSolidityPacked(types.toJS, values.jsify() as JSArray<JSAny>).toDart;
   }
 
-  static String encode(List<String> types, List<dynamic> values) {
+  /// Encode a list of values to an ABI encoded string of 32 bytes.
+  ///
+  /// Accepts a list of types and a list of values and returns an ABI encoded string.
+  /// Works the same as Solidity's `abi.encode` function.
+  ///
+  /// E.g encoding 10 -> 0x000000000000000000000000000000000000000000000000000000000000000a
+  String encode(List<String> types, List<dynamic> values) {
     final coder = JSEthersAbiCoder();
 
     // ignore: unnecessary_cast
     return coder.encode(types.toJS, _parseListToJSTypedList(values).jsify() as JSArray<JSAny>).toDart;
   }
 
-  static List<JSAny> _parseListToJSTypedList(List<dynamic> values) {
+  List<JSAny> _parseListToJSTypedList(List<dynamic> values) {
     // ignore: unnecessary_cast
     final List<JSAny> jsTypedArray = values.map((value) {
       if (value is BigInt) {
